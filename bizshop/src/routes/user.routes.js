070fbf9e9b393525a9190ce7e54b9742a208105d
[inum9] <inum9@outlook.com>
2025-07-20 16:47:20 +0530

    Implement refresh access token functionality: add refreshAccessToken route and logic to verify and generate new tokens.

--- bizshop/src/routes/user.routes.js
+++ bizshop/src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { login, logOut, signup } from "../controller/user.controller.js";
+import { login, logOut, refreshAccessToken, signup } from "../controller/user.controller.js";
 import { veriJwt } from "../middleware/Auth.middleware.js";
 const router= Router();
 router.route("/register").post(signup);
 router.route("/login").get(login);
+//secured routes
 router.route("/logout").post(veriJwt,logOut)
+router.route("/refresh-Access-token").put(veriJwt,refreshAccessToken);
 
 export{ router as userRoutes};
